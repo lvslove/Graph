41138e672ba21bd32078d41f9b98b548b3ddfed8
Your Name <segey.lalov.mma@icloud.com>
2020-09-30 17:43:21 +0400

    interface

--- graph.py
+++ graph.py
@@ -1,25 +1,26 @@
 import json
 from collections import defaultdict
+import copy
 
 
 class Graph(object):
 
-    def __init__(self, connections, directed=False):
+    def __init__(self, directed=False):
         self._graph = defaultdict(set)
         self._directed = directed
-        self.add_connections(connections)
+        #self.add_connections(connections)
 
-    def nice_print(self ):
-        s="\n"
+    def nice_print(self):
+        s = "\n"
         print(self._directed)
         for key in self._graph:
-            s+='\t{"'+str(key)+'"}:'  + str(self._graph[key])+"\n"
-
+            s += '\t{"' + str(key) + '"}:' + str(self._graph[key]) + "\n"
 
         print(s)
 
-    def test(self):
-        print (self._directed)
+    def copy(self):
+        s = copy.copy(self._graph)
+        return s
 
     def add_connections(self, connections):
 
@@ -35,16 +36,13 @@ class Graph(object):
     def add_Node(self, node1):
         self._graph[node1]
 
-
     def remove_Edge(self, node1, node2):
-
 
         self._graph[node1].remove(node2)
         if not self._directed:
             self._graph[node2].remove(node1)
 
     def remove_Node(self, node):
-
 
         for n, cxns in self._graph.items():
             try:
@@ -59,13 +57,10 @@ class Graph(object):
     def is_connected(self, node1, node2):
         return node1 in self._graph and node2 in self._graph[node1]
 
-
-
     def write_in_file(self):
         s = ""
         for key in self._graph:
-
-            s+= '"'+str(key)+'":' + str(self._graph[key])+","
+            s += '"' + str(key) + '":' + str(self._graph[key]) + ","
 
         newstring = s
         newstring = newstring.replace('}', ']')
@@ -91,7 +86,6 @@ class Graph(object):
             with open(file_path, 'w') as outfile:
                 json.dump(data, outfile)
 
-
     def update_file(self):
         def clear_json():
             file_path = 'file.json'
@@ -102,23 +96,23 @@ class Graph(object):
 
                 with open(file_path, 'w') as outfile:
                     json.dump(data, outfile)
+
         clear_json()
         self.write_in_file()
 
     def read_json(self):
         with open('creat.json', 'r', encoding='utf-8') as fh:  # открываем файл на чтение
             data = json.load(fh)
-        if (data['directed'])=="yes":
-            self._directed=True
+        if (data['directed']) == "yes":
+            self._directed = True
         else:
-            self._directed=False
+            self._directed = False
         for value in data['datas']:
-           s=(value)
+            s = (value)
         print(s)
-        z=""
+        z = ""
         for key in value:
-
-            z+= '"'+str(key)+'":' + str(value[key])+","
+            z += '"' + str(key) + '":' + str(value[key]) + ","
         newstring = z
         newstring = newstring.replace('}', ']')
         newstring = newstring.replace("'", '"')
@@ -129,8 +123,7 @@ class Graph(object):
         a = json.loads(newstring2)
         for n in a:
             self.add_Node(n)
-            i=0
-            while(len(a[n])>i):
-                self.add_Edge(n,a[n][i])
-                i+=1
-
+            i = 0
+            while (len(a[n]) > i):
+                self.add_Edge(n, a[n][i])
+                i += 1
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 from graph import Graph
+import copy
 
-defolt=[('1','4')]
+y=True
+while(y):
+    print("\n\t Work graph")
+    print("1. creat empty graph")
+    print("2. add node")
+    print("3. add edge")
+    print("4. remove node")
+    print("5. remove edge")
+    print("6. read json")
+    print("7. write json")
+    print("8. write console")
+    print("9. write exit")
+    print("write items\n")
+    n = int(input())
 
-g = Graph(defolt)
-g.read_json()
-g.nice_print()
-g.remove_Node('d')
-g.nice_print()
-g.remove_Node('BLAT')
-g.nice_print()
-g.update_file()
+    if(n==1):
+        g = Graph()
+    elif(n == 2):
+        print("Write node")
+        a=input()
+        g.add_Node(a)
+    elif (n == 3):
+        print("Write nodes")
+        a=input()
+        b=input()
+        g.add_Edge(a,b)
+    elif (n == 4):
+        print("Write node")
+        a = input()
+        g.remove_Node(a)
+    elif (n == 5):
+        print("Write nodes")
+        a = input()
+        b = input()
+        g.add_Edge(a,b)
+    elif (n == 6):
+        g.read_json()
+    elif (n == 7):
+        g.update_file()
+    elif (n == 8):
+        g.nice_print()
+    elif(n==9):
+        print("exit")
+        y=False
+    else:
+        print("ERROR")
+
+
+
 
 
